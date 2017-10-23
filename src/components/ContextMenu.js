6c018a5c239a50b8182f9ff36432dc25a84ba4de
Antonio <alvieirajr@gmail.com>
2017-10-23 20:43:27 -0300

    refactor: The Shelf component

---
+++ src/components/ContextMenu.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+ 
+class ContextMenu extends Component {
+    render() {
+        return (
+            <div>
+                <select>
+                    <option value="none" disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                </select>
+            </div>
+        )
+    }
+}
+
+export default ContextMenu;
