1d2727de078be2131f3b01cd75ada85bf04c5404
Antonio <alvieirajr@gmail.com>
2017-10-23 22:01:08 -0300

    refactor: The Shelf component pulling the book list array

--- src/components/Book.js
+++ src/components/Book.js
@@ -7,7 +7,7 @@ const Book = (props) => {
             <div className="book-top">                                                         
                 <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.image})`}}></div>
                 <div className="book-shelf-changer">
-                <ContextMenu/>
+                    <ContextMenu/>
                 </div>
             </div>
             <div className="book-title">{props.title}</div>
--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -3,26 +3,22 @@ import Book from './Book';
   
 class Shelf extends Component {
     render() {
+      console.log(this.props.books);
         return(
           <div>
             <div className="bookshelf">
             <h2 className="bookshelf-title">{this.props.title}</h2>
             <div className="bookshelf-books">
               <ol className="books-grid">
+               {this.props.books.map( (item) => (
                 <li>
                   <Book 
-                    title="To Kill a Mockingbird" 
-                    authors="Harper Lee"
-                    image="http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"
+                    title={item.title}
+                    authors={item.authors[0]}
+                    image={item.imageLinks.smallThumbnail}
                   />                  
                 </li>
-                <li>
-                  <Book 
-                    title="Ender's Game" 
-                    authors="Orson Scott Card"
-                    image="http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
-                  />                              
-                </li>
+               ))}
               </ol>
             </div>
           </div> 
