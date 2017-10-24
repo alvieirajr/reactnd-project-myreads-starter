0f415f8b3fc31c0862f7119f9e813b512ceb6de3
Antonio <alvieirajr@gmail.com>
2017-10-23 21:13:56 -0300

    refactor: The Book component

--- src/components/MyReads.js
+++ src/components/MyReads.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ContextMenu from './ContextMenu';
 import Shelf from './Shelf'
 import * as BooksAPI from '../BooksAPI';
  
@@ -22,30 +21,30 @@ class MyReads extends Component {
     });
   }
 
-    render() {
+  render() {
 
-        return (
-            <div>
+    return (
+        <div>
 
-            <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <Shelf title="Currently Reading"/>
-                <Shelf title="Want to Read"/>
-                <Shelf title="Read"/>                
-              </div>
-            </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
+        <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <Shelf title="Currently Reading"/>
+            <Shelf title="Want to Read"/>
+            <Shelf title="Read"/>                
           </div>
+        </div>
+        <div className="open-search">
+          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+        </div>
+      </div>
 
-            </div>
-        )
-    }
+        </div>
+    )
+  }
 }
 
 export default MyReads;
