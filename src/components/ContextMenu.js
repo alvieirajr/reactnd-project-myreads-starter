870a7500b5c22f17ef55aa2346a55fffbe991e8e
Antonio <alvieirajr@gmail.com>
2017-10-24 22:21:45 -0300

    fix: Bug on click into book-shelf-changer div

--- src/components/ContextMenu.js
+++ src/components/ContextMenu.js
@@ -3,15 +3,13 @@ import React, { Component } from 'react';
 class ContextMenu extends Component {    
     render() {
         return (
-            <div>
-                <select value={this.props.book.shelf} onChange={(event) => this.props.onChange(event.target.value, this.props.book)}>
-                    <option value="none" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                </select>
-            </div>
+            <select value={this.props.book.shelf} onChange={(event) => this.props.onChange(event.target.value, this.props.book)}>
+                <option value="none" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+            </select>
         )
     }
 }
